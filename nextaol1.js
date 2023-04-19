5c7a6b1decfad00c45a1ec9d326138e426be41a1
Nilson Filho <104315653+nrmendf@users.noreply.github.com>
2023-04-19 19:48:17 -0300

    Add files via upload

---
+++ nextaol1.js
@@ -0,0 +1,12 @@
+<script>
+var mensage;
+function load() {
+    mensage = prompt("Qual o seu nome?");
+
+    alert(`Seja Bem Vindo, ${mensage}`);
+}
+function touch() {
+    alert (mensage);
+}
+
+</script>
